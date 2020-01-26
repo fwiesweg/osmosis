5385680a7b0fce02c1428eb122b35fa79a57a53c
Florian Wiesweg <florian.wiesweg@campus.tu-berlin.de>
2020-01-26 14:15:46 +0100

    Prettify rendering

--- osmosis-render/src/main/java/org/openstreetmap/osmosis/render/v0_6/RenderTask.java
+++ osmosis-render/src/main/java/org/openstreetmap/osmosis/render/v0_6/RenderTask.java
@@ -2,19 +2,23 @@
 package org.openstreetmap.osmosis.render.v0_6;
 
 import java.awt.Color;
-import java.awt.Polygon;
+import java.awt.Dimension;
+import java.awt.geom.Path2D;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.nio.charset.Charset;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.stream.Collectors;
 import org.apache.batik.dom.GenericDOMImplementation;
 import org.apache.batik.svggen.SVGGraphics2D;
+import org.apache.commons.lang3.tuple.Pair;
 import org.openstreetmap.osmosis.core.container.v0_6.BoundContainer;
 import org.openstreetmap.osmosis.core.container.v0_6.EntityContainer;
 import org.openstreetmap.osmosis.core.container.v0_6.EntityProcessor;
@@ -39,9 +43,13 @@ public class RenderTask implements Sink {
     private final Map<Long, Node> nodes = new HashMap<>(500000);
 
     private final Document document;
+    private final int outputSizeX;
+    private final int outputSizeY;
 
-    public RenderTask(File outputFile) {
+    public RenderTask(File outputFile, int outputSizeX, int outputSizeY) {
         this.outputFile = outputFile;
+        this.outputSizeX = outputSizeX;
+        this.outputSizeY = outputSizeY;
 
         DOMImplementation domImpl = GenericDOMImplementation.getDOMImplementation();
         String svgNS = "http://www.w3.org/2000/svg";
@@ -59,25 +67,74 @@ public class RenderTask implements Sink {
 
     @Override
     public void complete() {
-        SVGGraphics2D svgGenerator = new SVGGraphics2D(document);
-        svgGenerator.setPaint(Color.red);
-        relations.values().stream()
+        Set<List<Pair<Double, Double>>> paths = relations.values().stream()
                 .map(r -> r.stream().map(ways::get).filter(w -> w != null).flatMap(w -> w.stream()))
-                .map(r -> {
-                    Polygon p = new Polygon();
-                    r
-                            .map(nid -> nodes.get(nid))
-                            .filter(n -> n != null)
-                            /*.map(n -> {
+                .map(r -> r
+                .map(nid -> nodes.get(nid))
+                .filter(n -> n != null)
+                .map(n -> {
+                    //Point p = Point.fromLatitudeLongitude(n.getLatitude(), n.getLongitude());
+                    //return Pair.of(p.getMeterX(), p.getMeterY());
+                    return Pair.of(n.getLatitude(), n.getLongitude());
+                })
+                .collect(Collectors.toList()))
+                .collect(Collectors.toSet());
+
+        double maxX = paths.stream()
+                .flatMap(p -> p.stream())
+                .map(p -> p.getLeft())
+                .collect(Collectors.maxBy(Comparator.naturalOrder()))
+                .get();
+        double minX = paths.stream()
+                .flatMap(p -> p.stream())
+                .map(p -> p.getLeft())
+                .collect(Collectors.minBy(Comparator.naturalOrder()))
+                .get();
+
+        double maxY = paths.stream()
+                .flatMap(p -> p.stream())
+                .map(p -> p.getRight())
+                .collect(Collectors.maxBy(Comparator.naturalOrder()))
+                .get();
+        double minY = paths.stream()
+                .flatMap(p -> p.stream())
+                .map(p -> p.getRight())
+                .collect(Collectors.minBy(Comparator.naturalOrder()))
+                .get();
+
+        double centerX = (maxX + minX) / 2;
+        double centerY = (maxY + minY) / 2;
+
+        double scaleX = outputSizeX / (maxX - minX);
+        double scaleY = outputSizeY / (maxY - minY);
+        double scale = Math.min(scaleX, scaleY);
+
+        SVGGraphics2D svgGenerator = new SVGGraphics2D(document);
+        svgGenerator.setSVGCanvasSize(new Dimension(outputSizeX, outputSizeY));
+
+        svgGenerator.setPaint(Color.red);
+        svgGenerator.rotate(-Math.PI / 2);
+        svgGenerator.translate(-Math.floor(outputSizeX / 2f), Math.floor(outputSizeY / 2f));
+
+        paths.stream().map(path -> {
+            Path2D.Double drawPath = new Path2D.Double();
+            path.stream()
+                    .map(p -> Pair.of((p.getLeft() - centerX) * scale, (p.getRight() - centerY) * scale))
+                    .peek(p -> {
+                        if (drawPath.getCurrentPoint() == null) {
+                            drawPath.moveTo(p.getLeft(), p.getRight());
+                        }
+                    })
+                    .skip(1)
+                    /*.map(n -> {
                                 return Point.fromLatitudeLongitude(n.getLatitude(), n.getLongitude());
                             })
                             .forEach(n -> p.addPoint((int) n.getMeterX() * 1000, (int) n.getMeterY() * 1000));*/
-                            .forEach(n -> {
-                                p.addPoint((int) (n.getLongitude() * 1000), (int) (n.getLatitude() * 1000));
-                            });
-                    return p;
-                })
-                .forEach(svgGenerator::fillPolygon);
+                    .forEach(p -> {
+                        drawPath.lineTo(p.getLeft(), p.getRight());
+                    });
+            return drawPath;
+        }).forEach(svgGenerator::draw);
 
         try ( FileOutputStream fos = new FileOutputStream(outputFile)) {
             svgGenerator.stream(new OutputStreamWriter(fos, Charset.forName("UTF-8")));
--- osmosis-render/src/main/java/org/openstreetmap/osmosis/render/v0_6/RenderTaskFactory.java
+++ osmosis-render/src/main/java/org/openstreetmap/osmosis/render/v0_6/RenderTaskFactory.java
@@ -12,6 +12,10 @@ public class RenderTaskFactory extends TaskManagerFactory {
     private static final String ARG_FILE_NAME = "file";
     private static final String DEFAULT_FILE_NAME = "output.svg";
 
+    private static final String ARG_SIZE_X = "sizex";
+    private static final String ARG_SIZE_Y = "sizey";
+    private static final int DEFAULT_SIZE = 1000;
+
     @Override
     protected TaskManager createTaskManagerImpl(TaskConfiguration taskConfig) {
         // Get the task arguments.
@@ -20,9 +24,12 @@ public class RenderTaskFactory extends TaskManagerFactory {
                 ARG_FILE_NAME,
                 getDefaultStringArgument(taskConfig, DEFAULT_FILE_NAME)
         );
+
+        int sizex = getIntegerArgument(taskConfig, ARG_SIZE_X, DEFAULT_SIZE);
+        int sizey = getIntegerArgument(taskConfig, ARG_SIZE_Y, DEFAULT_SIZE);
         File file = new File(fileName);
 
-        return new SinkManager(taskConfig.getId(), new RenderTask(file), taskConfig.getPipeArgs());
+        return new SinkManager(taskConfig.getId(), new RenderTask(file, sizex, sizey), taskConfig.getPipeArgs());
     }
 
 }
